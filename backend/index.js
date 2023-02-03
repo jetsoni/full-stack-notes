b3f91f274d24624045222ad2b0f413d8e987ad76
Samuli Laaksonen <samulilaaksonen1@gmail.com>
2023-02-03 09:45:53 +0200

    Ready to launch to the internet

--- backend/index.js
+++ backend/index.js
@@ -96,8 +96,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-//Note deleted *signed*
-const PORT = 3001
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
